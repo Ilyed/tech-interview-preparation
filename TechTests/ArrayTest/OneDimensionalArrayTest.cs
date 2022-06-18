c23fe59631dc3a0a86ef2fc4a5e6a0a1058bb625
Ilyesd <ilyesd@outlook.fr>
2022-06-19 01:33:55 +0200

    Create OneDimensionalArrayTest.cs
    Adding unit test for OneDimensionalArray.
    In particular, function ContainsDuplicate()

---
+++ TechTests/ArrayTest/OneDimensionalArrayTest.cs
@@ -0,0 +1,15 @@
+using TechExercices.Array;
+
+namespace TechTests.ArrayTest;
+
+public class OneDimensionalArrayTest
+{
+    [Fact]
+    public void ContainsDuplicateTest()
+    {
+        OneDimensionalArray testSubArray = new OneDimensionalArray();
+        int[] input = new[] { 1,2,3,1};
+        bool result = testSubArray.ContainsDuplicate(input);
+        Assert.True(result);
+    }
+}
