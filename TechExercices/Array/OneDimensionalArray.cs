cca1d4f8551e23703432f27b5dd70113af9bc595
Ilyesd <ilyesd@outlook.fr>
2022-06-19 01:04:28 +0200

    Optimised solution for Leetcode 217. Time : O(n) Space: O(n)

--- TechExercices/Array/OneDimensionalArray.cs
+++ TechExercices/Array/OneDimensionalArray.cs
@@ -3,18 +3,21 @@
 public class OneDimensionalArray
 {
     //Leetcode 217
-    public bool ContainsDuplicate(int[] nums) {
-        for (int i = 0; i < nums.Length-1; i++)
+    public bool ContainsDuplicate(int[] nums)
+    {
+        HashSet<int> numbers = new HashSet<int>();
+        for (int i = 0; i < nums.Length; i++)
         {
-            for (int j = i+1; j < nums.Length; j++)
+            if (numbers.Contains(nums[i]))
             {
-                if (nums[i] == nums[j])
-                {
-                    return true;
-                }
+                return true;
+            }
+            else
+            {
+                numbers.Add(nums[i]);
             }
         }
-
+        
         return false;
     } 
 }
