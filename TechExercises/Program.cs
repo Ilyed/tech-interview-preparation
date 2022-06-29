fbdf8e9bf12d9270ede158555cabb2dcf5ed7839
Ilyesd <ilyesd@outlook.fr>
2022-06-29 18:14:45 +0200

    First attempt at solving leetcode 3. Longest Substring Without Repeating Characters

--- TechExercises/Program.cs
+++ TechExercises/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using TechExercises.String;
+
 Console.WriteLine("Hello, World!");
+SubString subStringTest = new SubString();
+string stringToTest = "dvdf";
+int subStringLength = subStringTest.LengthOfLongestSubstring(stringToTest);
+Console.WriteLine($"SubString : {stringToTest} has a maximum subStringLength of: {subStringLength}");
---
+++ TechExercises/String/SubString.cs
@@ -0,0 +1,44 @@
+﻿namespace TechExercises.String;
+
+using System;
+
+public class SubString
+{
+    //3. Longest Substring Without Repeating Characters
+    public int LengthOfLongestSubstring(string s)
+    {
+        int length = s.Length - 1;
+        int maxLength = 0;
+        int temporaryMax = 0;
+        HashSet<char> subStringSet = new HashSet<char>();
+        if (String.IsNullOrEmpty(s))
+        {
+            return maxLength;
+        }
+
+        for (int i = 0; i <= length; i++)
+        {
+            for (int j = i; j <= length; j++)
+            {
+                char currentChar = s[j];
+                if (!subStringSet.Contains(currentChar))
+                {
+                    subStringSet.Add(currentChar);
+                    temporaryMax++;
+                }
+                else
+                {
+                    maxLength = Math.Max(temporaryMax, maxLength);
+                    j--;
+                    temporaryMax = 0;
+                    subStringSet = new HashSet<char>();
+                }
+            }
+
+            //We check again in case the last character made the substring bigger
+            maxLength = Math.Max(temporaryMax, maxLength);
+        }
+
+        return maxLength;
+    }
+}
