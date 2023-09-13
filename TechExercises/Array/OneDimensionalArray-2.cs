10310151f7eb777c24e5fa36f205e3d0f1335061
ilyes daou <ilyesd@outlook.fr>
2023-09-13 14:34:37 +0200

    redoing          //Leetcode 217

---
+++ TechExercises/Array/OneDimensionalArray-2.cs
@@ -0,0 +1,41 @@
+﻿namespace TechExercises.Array
+{
+    public static class OneDimensionalArrayBis
+    {
+        //Leetcode 217-bis
+        public static bool ContainsDuplicate(int[] nums)
+        {
+            var hashSet = new HashSet<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (hashSet.Contains(nums[i])) return true;
+                else
+                {
+                    hashSet.Add(nums[i]);
+                }
+            }
+
+            return false;
+          
+        }
+
+
+        //Leetcode 727
+        public static int RemoveDuplicates(int[] nums)
+        {
+            int numberOfElements = 0;
+            var hashSet = new HashSet<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (hashSet.Contains(nums[i])) return true;
+                else
+                {
+                    hashSet.Add(nums[i]);
+                }
+            }
+
+            return false;
+
+        }
+    }
+}
