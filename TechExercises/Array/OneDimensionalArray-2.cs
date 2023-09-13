fd103c5dd2078081fdb7e369eff60ce93037c1fc
ilyes daou <ilyesd@outlook.fr>
2023-09-13 14:55:58 +0200

    Leetcode 727

--- TechExercises/Array/OneDimensionalArray-2.cs
+++ TechExercises/Array/OneDimensionalArray-2.cs
@@ -1,9 +1,9 @@
 ﻿namespace TechExercises.Array
 {
-    public static class OneDimensionalArrayBis
+    public class OneDimensionalArrayBis
     {
         //Leetcode 217-bis
-        public static bool ContainsDuplicate(int[] nums)
+        public bool ContainsDuplicate(int[] nums)
         {
             var hashSet = new HashSet<int>();
             for (int i = 0; i < nums.Length; i++)
@@ -16,26 +16,25 @@
             }
 
             return false;
-          
         }
 
-
         //Leetcode 727
-        public static int RemoveDuplicates(int[] nums)
+        public int RemoveDuplicates(int[] nums)
         {
             int numberOfElements = 0;
+            int index = 0;
             var hashSet = new HashSet<int>();
             for (int i = 0; i < nums.Length; i++)
             {
-                if (hashSet.Contains(nums[i])) return true;
-                else
+                if (hashSet.Contains(nums[i])!)
                 {
                     hashSet.Add(nums[i]);
+                    nums[index] = nums[i];
+                    index++;
+                    numberOfElements++;
                 }
             }
-
-            return false;
-
+            return numberOfElements;
         }
     }
 }
